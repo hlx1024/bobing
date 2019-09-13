2952a3854ca208a34fb24b65c85c16daa1e77de5
hlx1024 <55269864+hlx1024@users.noreply.github.com>
2019-09-13 15:13:06 +0800

    Update bobing.py
    Add bobing result for different dice pattern

--- bobing.py
+++ bobing.py
@@ -1,17 +1,32 @@
 from random import randint
 j = 0
+def check(result):
+	for i in (1,2,3,5,6):
+		if result.count(i)>=5:
+			return 'ZhuangYuan'
+		if result.count(i)==4:
+			return 'SiJin'
+	if result == [1,2,3,4,5,6]:
+		return 'DuiTang'
+	num4 = result.count(4)
+	if num4 >= 4:
+		return 'ZhuangYuan'
+	if num4 == 3:
+		return 'SanHong'		
+	if num4 == 2:
+		return 'ErJu'
+	if num4 == 1:
+		return 'YiXiu'
+	return 'None'
 while True:
 	j+=1
 	result =[]
 	input('ROLL')
 	for i in range (6):
 		result.append(randint(1,6))
-	print(result)
-	if result.count(4)>=4:
-		print('ZhuangYuan. You have rolled %d times.' %j) 
+	print(result, end ='')
+	re = check(result)
+	print('  '+re)
+	if re == 'ZhuangYuan':
+		print('Congratulations. You have rolled %d times to get ZhuangYuan.' %j) 
 		quit()
-	else:
-		for i in (1,2,3,5,6):
-			if result.count(i)>=5:
-				print('ZhuangYuan. You have rolled %d times.' %j) 
-				quit()
